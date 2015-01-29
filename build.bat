9f954063013e62eb97f762c65209f14a6686d6a7
Douglas Thor <dougthor42@users.noreply.github.com>
2015-01-28 17:12:41 -0800

    Created build.py to replace build.bat. build.py includes putting entire dist directory into a .zip file.

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 SETLOCAL
 SET PATH=%PATH%;C:\WinPython27\python-2.7.6;C:\WinPython27\python-2.7.6\Scripts
-cxfreeze.bat "GDWCalc.py" --target-dir "dist\GDWCalc v1.5.2" -s --base-name=Win32GUI -O --compress --exclude-modules _gtkagg,_tkagg,bsddb,curses,email,pywin.debugger,pywin.debugger.dbgcon,pywin.dialogs,tcl,Tkconstants,Tkinter,scipy,pil
+cxfreeze.bat "GDWCalc.py" --target-dir "dist\GDWCalc v1.5.3" -s --base-name=Win32GUI -O --compress --exclude-modules _gtkagg,_tkagg,bsddb,curses,email,pywin.debugger,pywin.debugger.dbgcon,pywin.dialogs,tcl,Tkconstants,Tkinter,scipy,pil
+python bundle.py "dist\GDWCalc v1.5.3"
 ENDLOCAL
