3176ba7163a0acc625b9c1db436044ee1d50b499
Douglas Thor <dthor@transphormusa.com>
2019-10-14 08:52:48 -0700

    Update build_exe.bat to use py36.

--- build_exe.bat
+++ build_exe.bat
@@ -1,21 +1,21 @@
-call C:\WinPython34_x64\Scripts\env.bat
+call C:\WinPython3662\Scripts\env.bat
 cd C:\gitlab\dthor\GDWCalc
 
 echo ----- Deleting previous build -----
-RMDIR /S /Q C:\gitlab\dthor\GDWCalc\build\exe.win-amd64-3.4
+RMDIR /S /Q C:\gitlab\dthor\GDWCalc\build\exe.win-amd64-3.6
 
 echo ----- Running build script -----
 python build_exe.py build || goto :build_error
 
 echo ----- Launching Application -----
-cd .\build\exe.win-amd64-3.4\
+cd .\build\exe.win-amd64-3.6\
 GDWCalc.exe || goto :exe_error
 cd ..\..
 
 echo ----- Finished -----
 @echo off
 set size=0
-for /r %%x in (.\build\exe.win-amd64-3.4\*) do set /a size+=%%~zx
+for /r %%x in (.\build\exe.win-amd64-3.6\*) do set /a size+=%%~zx
 set /a mb = %size% / 1000000
 echo %size% Bytes
 echo %mb% MB
